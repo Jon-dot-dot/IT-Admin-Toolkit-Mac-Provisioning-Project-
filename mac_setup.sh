cb775ff1dd7f77cbabf0155757b396c85574c517
Jonathan L. Santana <106893265+Jon-dot-dot@users.noreply.github.com>
2025-11-28 03:19:41 -0800

    Add files via upload

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,31 +1,77 @@
-#!/bin/bash
+#/bin/bash
 
-echo "Starting MacOS provisioning..."
+echo "Starting MacOS setup..."
 
-#step 1: Install Homebrew if not installed
-if ! command -v brew &> /dev/null
-then 
-	echo "Homebrew not found. Installing Homebrew..."
+#Function: Loads Homebrew into current shell
+
+load_brew() { 
+	if [ -x /opt/homebrew/bin/brew ]; then
+		echo "Loading Homebrew (Apple Silicon)..."
+		eval "$(/opt/homebrew/bin/brew shellenv)"
+	elif [ -x /usr/local/bin/brew ]; then
+		echo "Loading Homebrew (Intel mac)..."
+		eval "$(/usr/local/bin/brew shellenv)"
+	fi
+   }
+
+#This checks if Homebrew is installed
+if ! command -v brew &>/dev/null; then
+	echo "Homebrew not found - installing Homebrew..."
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+	echo "Loading Homebrew after installation..."
+	load_brew
 else
-	echo "Homebrew already installed."
+	echo "Homebrew is already installed."
+	load_brew
 fi
 
-#Step2: Update Brew
-brew update
+#This verifies if brew is working
+
+if ! command -v brew &>/dev/null; then
+	echo "ERROR: Homebrew still not found. PATH issue."
+	echo "Try restarting your terminal and re-running this script."
+	exit 1
+fi
+
+echo "Homebrew detected: $(brew --version)"
 
 
-#Step 3: Install required applications
+#Install packages
+echo "Installing essential packages..."
+
 brew install --cask google-chrome
-brew install --cask slack
-brew install --cask zoom
 brew install --cask visual-studio-code
+brew install --cask zoom
+brew install --cask git
+brew install --cask wget
+brew install -cask htop
 
 
-echo "All applications installed!"
+echo "Completed Software installation!"
 
 
-#Step 4: Enable firewall
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+echo "MacOS setup script finished successfully."
 
-echo "Provisioning Complete!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
